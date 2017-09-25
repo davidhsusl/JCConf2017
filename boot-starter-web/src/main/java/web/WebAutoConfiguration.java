c0abbb3bd38d8257fad6de1d3e234fe5e520eb38
David Hsu <david.hsu.sl@gmail.com>
2017-09-25 18:23:16 +0800

    update

--- boot-starter-web/src/main/java/web/WebAutoConfiguration.java
+++ boot-starter-web/src/main/java/web/WebAutoConfiguration.java
@@ -6,13 +6,7 @@ import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomi
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.ConfigurableEnvironment;
-import org.springframework.core.env.MapPropertySource;
-import org.springframework.core.env.MutablePropertySources;
-import org.springframework.util.SocketUtils;
-
-import java.util.HashMap;
-import java.util.Map;
+import web.registrar.EmbeddedServletContainerCustomizerImpl;
 
 /** @author David Hsu */
 @EnableConfigurationProperties(ServerProperties.class)
@@ -21,20 +15,9 @@ public class WebAutoConfiguration {
 
   @Autowired private ServerProperties properties;
 
-  @Autowired private ConfigurableEnvironment environment;
-
   @ConditionalOnProperty(name = "server.port.random.enable", havingValue = "true")
   @Bean
   public EmbeddedServletContainerCustomizer containerCustomizer() {
-    return container -> {
-      int port = SocketUtils.findAvailableTcpPort(properties.getMin(), properties.getMax());
-
-      MutablePropertySources propertySources = environment.getPropertySources();
-      Map<String, Object> serverMap = new HashMap<>();
-      serverMap.put("server.port", port);
-      propertySources.addFirst(new MapPropertySource("CUSTOMIZE_SERVER_PROPS", serverMap));
-
-      container.setPort(port);
-    };
+    return new EmbeddedServletContainerCustomizerImpl(properties.getMin(), properties.getMax());
   }
 }
